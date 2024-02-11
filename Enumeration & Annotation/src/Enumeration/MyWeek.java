eeddcc04fcb7a52aa78a721ee1fc5682455a46c8
PRAVEEN <PRAVEEN@DESKTOP-T94BTI2>
2024-02-11 20:35:24 +0000

    11-02-24

--- Enumeration & Annotation/src/Enumeration/MyWeek.java
+++ Enumeration & Annotation/src/Enumeration/MyWeek.java
@@ -10,9 +10,11 @@ public class MyWeek {
 
 	public static void main(String[] args) {
 
+		System.out.println("1st Days of week : "+Days.Sunday);
         System.out.println("2nd Days of week : "+Days.Monday);
         System.out.println("4nd Days of week : "+Days.Wednesday);
         System.out.println("6nd Days of week : "+Days.Friday);
+      
 	}
 
 }
