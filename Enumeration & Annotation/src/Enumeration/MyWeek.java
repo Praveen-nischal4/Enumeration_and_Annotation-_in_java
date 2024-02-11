2b9735ad25592228eff304ea463c3390afe43e22
PRAVEEN <PRAVEEN@DESKTOP-T94BTI2>
2024-02-11 20:13:16 +0000

    11-02-23

---
+++ Enumeration & Annotation/src/Enumeration/MyWeek.java
@@ -0,0 +1,18 @@
+//Accesing enumeration method first.
+package Enumeration;
+
+enum Days
+{
+	Sunday,Monday,Tuesday,Wednesday,Thursday,Friday,Saturday          //declare enumeration
+}
+
+public class MyWeek {
+
+	public static void main(String[] args) {
+
+        System.out.println("2nd Days of week : "+Days.Monday);
+        System.out.println("4nd Days of week : "+Days.Wednesday);
+        System.out.println("6nd Days of week : "+Days.Friday);
+	}
+
+}
